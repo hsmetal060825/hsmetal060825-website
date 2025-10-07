1dded7bdc1199d97d69b592292eedb5404ad3044
hsmetal060825 <hsmetal060825@gmail.com>
2025-10-07 20:05:01 +0800

    1007_02

--- components/ui/button.tsx
+++
@@ -1,7 +0,0 @@
-
-import * as React from "react";
-type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & { variant?: "gold" | "outline" };
-export function Button({ variant="gold", className, ...props }: Props) {
-  const styles = variant==="gold" ? "btn btn-gold" : "btn btn-outline";
-  return <button className={`${styles} ${className||""}`} {...props} />;
-}
--- components/ui/card.tsx
+++
@@ -1,6 +0,0 @@
-
-import * as React from "react";
-export function Card(props: React.HTMLAttributes<HTMLDivElement>) { return <div {...props} className={`card ${props.className||""}`} /> }
-export function CardHeader(props: React.HTMLAttributes<HTMLDivElement>) { return <div {...props} className={`p-0 ${props.className||""}`} /> }
-export function CardContent(props: React.HTMLAttributes<HTMLDivElement>) { return <div {...props} className={`p-6 ${props.className||""}`} /> }
-export function CardTitle(props: React.HTMLAttributes<HTMLHeadingElement>) { return <h3 {...props} className={`text-lg font-semibold ${props.className||""}`} /> }
